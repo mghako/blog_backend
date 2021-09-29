44c14893bc55b5da0b6b7216d9cf0ba47e3f8083
htet aung ko <htetaungko@yahoo.com>
2021-09-29 14:23:42 +0630

    adding cache for blog post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,7 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));
     }
     public function show(Post $post) {
+        cache()->remember('post'.$post->id, 3600,)
         return view('posts.show', compact('post'));
     }
 
