d08016cc5f6c9bc56011ec61696be39d6324d923
htet aung ko <htetaungko@yahoo.com>
2021-11-11 17:45:05 +0630

    deleted content in post view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
         @foreach($posts as $post)
         <article class="flex flex-col shadow my-4">
             <a href="#" class="hover:opacity-75">
-                <img src="{{asset('storage/'. $post->thumbnail)}}">
+                <img src="{{asset('storage/'. $post->thumbnail)}}" class="w-full">
             </a>
             <div class="bg-white flex flex-col justify-start p-6">
                 <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">{{$post->category->name}}</a>
@@ -12,7 +12,6 @@
                 <p href="#" class="text-sm pb-3">
                     By <a href="#" class="font-semibold hover:text-gray-800">{{ $post->author->name }}</a>, Published on {{ $post->published_at->diffForHumans() }}
                 </p>
-                <a href="#" class="pb-6">{!! substr($post->content, 0, 101) . '...' !!}</a>
                 <a href="{{ route('posts.show', $post->slug) }}" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
             </div>
         </article>
